5fc479358784fc8ab64c65c01013c3e196b56dad
Michiel Schukking <l.m.schukking@gmail.com>
2016-08-11 19:49:34 +0200

    Add the ability to create users through the admin backend
    (users that can’t login…)

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,26 @@
+class Admin::UsersController < Admin::ApplicationController
+  def index
+    @users = User.order(:email)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = "Gebruiker is succesvol toegevoegd."
+      redirect_to admin_users_path
+    else
+      flash.now[:alert] = "Gebruiker is niet toegevoegd."
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
---
+++ spec/features/admin/creating_users_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.feature 'Admins can create new users', type: :feature do
+  let(:admin) { FactoryGirl.create(:user, :admin) }
+
+  before do
+    login_as(admin)
+    visit '/'
+    click_link 'Admin'
+    click_link 'Gebruikers'
+    click_link 'Nieuwe Gebruiker'
+  end
+
+  scenario 'with valid credentials' do
+    fill_in 'E-mail', with: 'newbie@example.com'
+    fill_in 'Wachtwoord', with: 'password'
+    click_button 'Toevoegen'
+
+    expect(page).to have_content 'Gebruiker is succesvol toegevoegd.'
+  end
+
+end
