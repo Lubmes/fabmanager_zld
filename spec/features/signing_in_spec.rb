2039a26934960e145c2b86414830ca2b21166cca
Michiel Schukking <l.m.schukking@gmail.com>
2016-08-12 13:27:24 +0200

    Archived users can not sign in to the app

--- spec/features/signing_in_spec.rb
+++ spec/features/signing_in_spec.rb
@@ -13,4 +13,16 @@ RSpec.feature "Users can sign in", type: :feature do
     expect(page).to have_content "U bent succesvol ingelogd."
     expect(page).to have_content "Ingelogd als #{user.email}"
   end
+
+  scenario "unless they are archived" do
+    user.archive
+
+    visit "/"
+    click_link "Inloggen"
+    fill_in "E-mail", with: user.email
+    fill_in "Wachtwoord", with: "password"
+    click_button "Login"
+
+    expect(page).to have_content "Uw account is gearchiveerd."
+  end
 end
