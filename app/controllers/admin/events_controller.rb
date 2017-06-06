6d5e9bc99ceaf8ff85afd06f90229665e328e6f2
Liam "Andreas Hendrik" Kisters <LiamAHK@live.nl>
2017-06-06 19:55:46 +0200

    Auto stash before merge of "Events" and "origin/Events"
    Bespreking donderdag
    alle pagina's bespreken.
    Open inloop aparte webapplicatie shortcut op de desktop.

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,5 +1,23 @@
 class Admin::EventsController < ApplicationController
+  before_action :set_event, only: [:approved, :show, :edit, :update, :destroy]
+
     def index
-  @events = Event.all
+      @events = Event.all
+    end
+
+    def approved
+      @event.approved = true
+      @event.save!
+    end
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def event_params
+      params.require(:event).permit(:title, :description, :start_time, :end_time, :machine_ids => [])
     end
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,7 +5,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all.where(approved: false)
+    @events = Event.all.where(approved: true)
   end
 
   # GET /events/1
