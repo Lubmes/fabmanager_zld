f6e32671d4fc410d1df5709304380e5028d986ad
Liam "Andreas Hendrik" Kisters <LiamAHK@live.nl>
2017-06-09 16:56:42 +0200

    Added some stufffss

--- app/mailers/events_mailer.rb
+++ app/mailers/events_mailer.rb
@@ -7,6 +7,12 @@ class EventsMailer < ApplicationMailer
     mail(to: @user.email, subject: 'Huur verzoek aangemaakt.')
   end
 
+  def event_approve_email(user, event)
+    @user = user
+    @event = event
+    mail(to: @user.email, subject: 'Huur verzoek is geaccepteerd.')
+  end
+
   def user_register(user)
     @user = user
     mail(to: @user.email, subject: 'Succesvol geregistreerd')
